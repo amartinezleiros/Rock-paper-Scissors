0f09e6aafd5be9c0852eed13a359f144fb6d294a
Aula <Aula@EQUIP079579.xunta.local>
2020-10-19 10:14:50 +0200

    v_02

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/Aula/Documents/Oscar/Ejercicios Practicos/Ejercicios unidad06/Rock-paper-Scissors
+# Build directory: C:/Users/Aula/Documents/Oscar/Ejercicios Practicos/Ejercicios unidad06/Rock-paper-Scissors/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
