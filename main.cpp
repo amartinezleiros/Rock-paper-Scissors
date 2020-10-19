4ad21655fb7e4ac14fa59061b87f923894b4f56e
Aula <Aula@EQUIP079578.xunta.local>
2020-10-19 11:54:32 +0200

    v3.1

--- main.cpp
+++ main.cpp
@@ -1,6 +1,24 @@
 #include <iostream>
+#include "jugador.h"
+#include "jugador1.h"
+#include "jugadorIA.h"
 
-int main(int, char**) {
+int main() {
+
+    jugador1 j1; //INSTANCIA
+
+    j1.jugar();
+
+
+    jugadorIA jIA; //INSTANCIA
+
+    jIA.jugar();
+
+    
+
+
+
+    
 
 
 }
